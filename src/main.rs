20e75c04b8ebbc8ea3ebe0a9c44b5790bb836256
web3ten0 <web3ten0@gmail.com>
2022-01-18 21:30:34 +0900

    added docker-compose settings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, Responder, HttpResponse, HttpServer};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+      .bind("0.0.0.0:8080")?
+      .run()
+      .await
 }
